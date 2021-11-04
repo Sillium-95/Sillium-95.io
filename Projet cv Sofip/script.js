e6585a17dbdc977944ba656be38bd184026eb0ba
Hrosso Alexandre <89102292+Sillium-95@users.noreply.github.com>
2021-11-04 15:46:12 +0100

    Add files via upload

---
+++ Projet cv Sofip/script.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function(){
+	AOS.init({ disable: 'mobile' });
+});
+
+/* For mobile support, delet the 'mobile' */
+
+<script>
+    AOS.init();
+  </script>;
+
+
+
+/* prpgress bar */
+
+
